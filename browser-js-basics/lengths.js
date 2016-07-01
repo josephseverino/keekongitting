1e310b1f98095594c8f1a24d6e5472869c38ec33
Joseph Severino <joseph.severino@colorado.edu>
2016-07-01 20:57:55 +0000

    finished lengths function

---
+++ browser-js-basics/lengths.js
@@ -0,0 +1,14 @@
+var array = [];
+var words = ["hello","what","is","up","dude"];
+function lengths(value){    
+    
+      
+     for (var i=0; i < value.length; i++){
+     
+     array.push(words[i].length) 
+   
+}
+
+}
+lengths(words);
+console.log(array);
