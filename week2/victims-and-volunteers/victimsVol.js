d3c96b661ecb55107b8ea24cf754577d69a2f52f
Joseph Severino <joseph.severino@colorado.edu>
2016-07-05 22:07:59 +0000

    This one needs some fine tuning

---
+++ week2/victims-and-volunteers/victimsVol.js
@@ -0,0 +1,15 @@
+var userInput = prompt("How many disaster victims do you have");
+
+var names = {};
+var phoneNumbers = {};
+var street = {};
+
+for(var i=0; i < userInput; i++){
+    names.push(prompt("What's the name?"));
+  
+   
+} 
+
+
+
+
